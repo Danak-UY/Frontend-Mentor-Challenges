4b9112c9c58e6ec4336a04fcc42f40a6097a092f
Danak-UY <37196682+Danak-UY@users.noreply.github.com>
2020-08-11 00:51:24 -0300

    Project build

---
+++ 08_social-media-dashboard/build/precache-manifest.6731743d191b2f49386913b7a0beaccd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd47c0e316e8e2e6c75ada36305802a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "096f5f7abaa4950d464e",
+    "url": "/static/css/main.de0369f5.chunk.css"
+  },
+  {
+    "revision": "8d5aa390d43bd3780df0",
+    "url": "/static/js/2.a6fc6bd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6fc6bd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096f5f7abaa4950d464e",
+    "url": "/static/js/main.0b5ff7ff.chunk.js"
+  },
+  {
+    "revision": "d100a554f43d16da85e2",
+    "url": "/static/js/runtime-main.fc7ac124.js"
+  },
+  {
+    "revision": "69a5c77f51678c145d64af371ff73b22",
+    "url": "/static/media/icon-down.69a5c77f.svg"
+  },
+  {
+    "revision": "a738376499ac8a902fd2ce03c3030f98",
+    "url": "/static/media/icon-facebook.a7383764.svg"
+  },
+  {
+    "revision": "991487fe03dace6f69a870d3085aadf1",
+    "url": "/static/media/icon-instagram.991487fe.svg"
+  },
+  {
+    "revision": "810ccee84b0132ca7921a1b0b4ee6021",
+    "url": "/static/media/icon-twitter.810ccee8.svg"
+  },
+  {
+    "revision": "95f916a549511a0912328d5626522d2c",
+    "url": "/static/media/icon-up.95f916a5.svg"
+  },
+  {
+    "revision": "8499da129aedb70f3e20262951f7e236",
+    "url": "/static/media/icon-youtube.8499da12.svg"
+  }
+]);
